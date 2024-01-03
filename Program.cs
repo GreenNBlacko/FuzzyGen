b3fd1cbb496aa16c61bc1f2ea6c0259cd6087fe5
s053546 <s053546@EIF-D-S-207-5>
2024-01-03 10:12:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Provide x: "); var x = int.Parse(Console.ReadLine()); /*       x - phrase length     */
+Console.Write("Provide y: "); var y = int.Parse(Console.ReadLine()); /*     y - min freq (y; inf)   */
+
+string outStr = "";
+
+List<string> phrases = new();
+
+Console.Write("Provide n: "); var n = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < n; i++) {
+    for(int o = 0; o < x; o++) {
+
+    }
+}
