fcfb8459e8ad97b0d57ac8b2feab631898deb8a2
G R A Y S C A L E <grayscale@DESKTOP-7R7SPLS>
2024-02-04 21:40:30 +0200

    changed x from an absolute value to an interval

--- Program.cs
+++ Program.cs
@@ -2,14 +2,15 @@
 /*     Text file generator for the fuzzy search algorithm     */
 /*                                                            */
 
+using System.Diagnostics.Metrics;
 using System.Text;
 
 var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
 var rand = new Random((int)DateTime.Now.Ticks);
 
-Console.Write("Provide x: "); var x = int.Parse(Console.ReadLine()); // x - phrase length
-Console.Write("Provide y: "); var y = int.Parse(Console.ReadLine()); // y - min freq (y; inf)
+Console.Write("Provide x: "); var x = int.Parse(Console.ReadLine()); // x - min phrase length [x; inf)
+Console.Write("Provide y: "); var y = int.Parse(Console.ReadLine()); // y - min freq [y; inf)
 
 Console.Write("Provide n: "); var n = int.Parse(Console.ReadLine()); // n - phrase count
 Console.Write("Provide m: "); var m = int.Parse(Console.ReadLine()); // m - final element count
@@ -23,7 +24,7 @@ List<string> phrases = new();
 for (int i = 0; i < n; i++) {                                        // Generating the phrases
 	var phrase = "";
 
-	for (int o = 0; o < x; o++) {
+	for (int o = 0; o < x + rand.Next(0, (x + 3) % m); o++) {
 		phrase += chars[rand.Next(chars.Length)];
 	}
 
@@ -39,9 +40,10 @@ for (int i = 0; i < m; i++) {                                        // Generati
 }
 
 List<int> keys = new();
+var counts = new Dictionary<string, int>();
 
 for (int i = 0; i < n; i++) {                                        // adding the phrases into the text
-	var rep = rand.Next(y, y + 2);                                   // how much will a certain phrase repeat itself
+	var rep = rand.Next(y, y + 10);                                   // how much will a certain phrase repeat itself
 
 	for (int o = 0; o < rep; o++) {                                  // placing the phrase with the frequency of (y; y+2] 
 		var pos = rand.Next(0, m - 1 - x);                           // the index to place the current phrase
@@ -61,33 +63,23 @@ for (int i = 0; i < n; i++) {                                        // adding t
 			pos = rand.Next(0, m - 1 - x);
 		}
 
-		stringBuilder.Remove(pos, x);
+		stringBuilder.Remove(pos, phrases[i].Length);
 		stringBuilder.Insert(pos, phrases[i]);
 
 		keys.Add(pos);
 	}
+
+	counts[phrases[i]] = rep;
 }
 
 outStr = stringBuilder.ToString();
 
 keys.Sort();
 
-var counts = new Dictionary<string, List<int>>();
+Console.WriteLine("Input string:\n" + outStr + "\nExpected output(inaccurate):");
 
-foreach (var phrase in phrases) {
-	counts.Add(phrase, new());
-}
-
-Console.WriteLine("Input string:\n" + outStr + "\nExpected output:");
-foreach (var key in keys) {
-	counts[outStr.Substring(key, x)].Add(key);
-}
-
-var names = new List<string>(counts.Keys);
-names.Sort();
-
-foreach (var name in names) {
-	Console.WriteLine(string.Format("x{0} {1}", counts[name].Count, name));
+foreach (var (name, count) in counts) {
+	Console.WriteLine(string.Format("x{0} {1}", count, name));
 }
 
 /*									END OF TEST STRING GENERATION									*/
@@ -98,19 +90,37 @@ Console.WriteLine();
 
 Dictionary<string, int> matches = new();
 List<string> valuesToKeep = new();
-StringBuilder bufferBuilder = new(x);
 
-for (int i = 0; i < outStr.Length - x; i++) {
-	bufferBuilder.Clear().Append(outStr, i, x);
-	var buffer = bufferBuilder.ToString();
+for (int size = outStr.Length - 1; size > x; size--) {
+	StringBuilder bufferBuilder = new(size);
 
-	if (!matches.TryGetValue(buffer, out var count))
-		count = 0;
+	int tempCount = matches.Count;
 
-	matches[buffer] = count + 1;
+	for (int i = 0; i < outStr.Length - size; i++) {
+		bufferBuilder.Clear().Append(outStr, i, x);
+		var buffer = bufferBuilder.ToString();
 
-	if (matches[buffer] >= y && !valuesToKeep.Contains(buffer))
-		valuesToKeep.Add(buffer);
+		if (!matches.TryGetValue(buffer, out var count))
+			count = 0;
+
+		matches[buffer] = count + 1;
+
+		if (matches[buffer] >= y && !valuesToKeep.Contains(buffer)) {
+			bool write = true;
+
+			for(int o = 0; o < valuesToKeep.Count && write; o++) {
+				if (valuesToKeep[o].Contains(buffer))
+					write = false;
+			}
+
+			if(write)
+				valuesToKeep.Add(buffer);
+		}
+			
+	}
+
+	if (tempCount == matches.Count && valuesToKeep.Count != 0)
+		break;
 }
 
 valuesToKeep.Sort();
